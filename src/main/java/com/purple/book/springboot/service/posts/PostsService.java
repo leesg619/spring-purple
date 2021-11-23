74a27f57c0e2108033a75e4da2270f8dd401e479
leesg619 <gkwoap@nate.com>
2021-11-23 15:34:46 +0900

    Post (게시물) 등록 API

---
+++ src/main/java/com/purple/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.purple.book.springboot.service.posts;
+
+import com.purple.book.springboot.domain.posts.PostsRepository;
+import com.purple.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
