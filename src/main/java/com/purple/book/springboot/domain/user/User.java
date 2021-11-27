f60fd0782be5ebf326fab2e232d86f21b49ce3d9
leesg619 <gkwoap@nate.com>
2021-11-27 19:44:59 +0900

    User domain,Repo 추가

---
+++ src/main/java/com/purple/book/springboot/domain/user/User.java
@@ -0,0 +1,49 @@
+package com.purple.book.springboot.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String pictrue) {
+        this.name = name;
+        this.picture = pictrue;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
